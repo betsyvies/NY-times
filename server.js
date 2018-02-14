f57a399a4d0889b37010a6a22a41fe4cd4a5b409
betsyvies <ing.betsyvies@gmail.com>
2018-02-13 23:10:12 -0500

    añadiendo carpetas

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+const server = app.listen(3000, encender);
+function encender() {
+  console.log('Servidor encendido');
+}
+
+app.use(express.static('public')); 
