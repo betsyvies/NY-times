e53247f8fe10ccb695999f75ab27f91a1bf01c01
betsyvies <ing.betsyvies@gmail.com>
2018-02-14 04:50:10 -0500

    añadiendo ambas funciones

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,38 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
 
+// Inicializamos al objeto en una variable
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+});
 
+function getNews() {
+  const articleRequest =  new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=347d672874fd431eb13860e1a3f4d50b`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+
+function handleError() {
+  console.log('An error has occurred');
+}
+
+function addNews() {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerHTML = snippet;
+
+  responseContainer.appendChild(li);
+}
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const server = app.listen(3000, encender);
-function encender() {
-  console.log('Servidor encendido');
+const server = app.listen(3000, turnOn);
+
+function turnOn() {
+  console.log('Server on');
 }
 
 app.use(express.static('public')); 
